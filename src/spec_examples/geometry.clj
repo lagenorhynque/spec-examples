7fb1c915303f2bc85d18fa435c99267ea91e4f54
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-09-09 03:14:42 +0900

    implement area function

--- src/spec_examples/geometry.clj
+++ src/spec_examples/geometry.clj
@@ -17,14 +17,28 @@
 
 (s/def ::shape (s/multi-spec shape-type ::type))
 
+(s/fdef area
+        :args (s/cat :shape ::shape)
+        :ret double?)
+
 (s/fdef volume
         :args (s/cat :shape ::shape)
         :ret double?)
 
+(defmulti area ::type)
+(defmethod area ::sphere [{::keys [radius]}]
+  (* 4 Math/PI (Math/pow radius 2)))
+(defmethod area ::cube [{::keys [side-a]}]
+  (* 6 (Math/pow side-a 2)))
+(defmethod area ::cuboid [{::keys [side-a side-b side-c]}]
+  (* 2 (+ (* side-a side-b)
+          (* side-b side-c)
+          (* side-c side-a))))
+
 (defmulti volume ::type)
 (defmethod volume ::sphere [{::keys [radius]}]
-  (* 4/3 Math/PI radius radius radius))
+  (* 4/3 Math/PI (Math/pow radius 3)))
 (defmethod volume ::cube [{::keys [side-a]}]
-  (* side-a side-a side-a))
+  (Math/pow side-a 3))
 (defmethod volume ::cuboid [{::keys [side-a side-b side-c]}]
   (* side-a side-b side-c))
