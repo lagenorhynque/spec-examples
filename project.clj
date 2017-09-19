fd4d404d1c62c0b158491bc128206bdc391e8513
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-09-19 13:04:50 +0900

    Bump up clojure version to 1.9.0-beta1

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject spec-examples "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "clojure.spec examples"
+  :url "https://github.com/lagenorhynque/spec-examples"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]]
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]]
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
   :java-source-paths ["src/java"]
