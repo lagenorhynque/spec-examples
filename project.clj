99b724886249e1287a6f5c652d4e31544d8e4fc7
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-12-03 03:59:44 +0900

    update dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/lagenorhynque/spec-examples"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta1"]]
+  :dependencies [[org.clojure/clojure "1.9.0-RC2"]]
   :source-paths ["src/clj"]
   :test-paths ["test/clj"]
   :java-source-paths ["src/java"]
